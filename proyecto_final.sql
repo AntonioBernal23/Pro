78a856444be8ff9428c5c3e9f652d48b22ea8af1
AntonioBernal23 <antonio.bernal7733@alumnos.udg.mx>
2025-04-16 21:08:22 -0600

    Ya se registran alumnos a las materias correctamente(subi el sql)

--- proyecto_final.sql
+++ proyecto_final.sql
@@ -28,13 +28,12 @@ CREATE TABLE `materia_usuario` (
   `id_maestro` int DEFAULT NULL,
   `id_alumno` int DEFAULT NULL,
   PRIMARY KEY (`id_materia_usuario`),
-  UNIQUE KEY `unique_maestro_materia` (`id_materia`,`id_maestro`),
+  UNIQUE KEY `unique_alumno_materia` (`id_alumno`,`id_materia`),
   KEY `materia_usuario_ibfk_2` (`id_maestro`),
-  KEY `materia_usuario_ibfk_3` (`id_alumno`),
-  CONSTRAINT `materia_usuario_ibfk_1` FOREIGN KEY (`id_materia`) REFERENCES `materias` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `materia_usuario_ibfk_2` FOREIGN KEY (`id_maestro`) REFERENCES `usuarios` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  KEY `materia_usuario_ibfk_1` (`id_materia`),
+  CONSTRAINT `materia_usuario_ibfk_1` FOREIGN KEY (`id_materia`) REFERENCES `materias` (`ID`) ON DELETE CASCADE,
   CONSTRAINT `materia_usuario_ibfk_3` FOREIGN KEY (`id_alumno`) REFERENCES `usuarios` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,7 +42,7 @@ CREATE TABLE `materia_usuario` (
 
 LOCK TABLES `materia_usuario` WRITE;
 /*!40000 ALTER TABLE `materia_usuario` DISABLE KEYS */;
-INSERT INTO `materia_usuario` VALUES (1,1,8,NULL);
+INSERT INTO `materia_usuario` VALUES (17,5,2,3);
 /*!40000 ALTER TABLE `materia_usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -64,7 +63,7 @@ CREATE TABLE `materias` (
   `hora_comienzo` time DEFAULT NULL,
   `hora_fin` time DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -73,7 +72,7 @@ CREATE TABLE `materias` (
 
 LOCK TABLES `materias` WRITE;
 /*!40000 ALTER TABLE `materias` DISABLE KEYS */;
-INSERT INTO `materias` VALUES (1,'Algebra','h23','14','matematicas','miercoles','11:00:00','13:00:00');
+INSERT INTO `materias` VALUES (5,'Ecuaciones','H93','0','algebra','lunes','08:00:00','10:00:00');
 /*!40000 ALTER TABLE `materias` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -94,7 +93,7 @@ CREATE TABLE `usuarios` (
   PRIMARY KEY (`ID`),
   UNIQUE KEY `usuario` (`usuario`),
   UNIQUE KEY `correo` (`correo`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -103,7 +102,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1,'Tony','tony','tony@gmail.com','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','administrador'),(2,'Jesus','jesus','jesus@gmail.com','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','maestro'),(3,'Pedro','pedro','pedro@gmail.com','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','alumno'),(5,'Guadalupe','lupita','lupita@gmail.com','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','administrador'),(6,'sthefania','nia','nia@gmail.com','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','administrador'),(7,'daniel','daniel','daniel@gmail.com','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','alumno'),(8,'Frijol','frijol','frijol@gmail.com','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','maestro');
+INSERT INTO `usuarios` VALUES (1,'Tony','tony','tony@gmail.com','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','administrador'),(2,'Jesus','jesus','jesus@gmail.com','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','maestro'),(3,'Pedro','pedro','pedro@gmail.com','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','alumno'),(9,'Frijol','frijol','frijol@gmail.com','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','alumno');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -116,4 +115,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-04-16  3:23:34
+-- Dump completed on 2025-04-17  3:06:41
